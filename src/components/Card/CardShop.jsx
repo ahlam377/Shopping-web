2152aba0fbd35e0a85a8b09e182e987614dca065
ahlam <ahlamzjaber2@gmail.com>
2023-02-17 18:36:33 +0200

    create shopping web

---
+++ src/components/Card/CardShop.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function CardShop({id}) {
+  return (
+    <NavLink to={`product/${id}`} className="col-12 col-md-6 px-3 text-decoration-none">
+      <div className="bg-white mb-3 p-3 p-md-5 position-relative">
+        <span className="heart position-absolute">
+          <i class="fa-regular fa-heart"></i>
+        </span>
+        <div className="h-375 mb-3">
+          <img src="" alt="not" />
+        </div>
+        <h3 className="text-center fw-bold">Urbano Jacket</h3>
+        <div className="rating my-3 d-flex justify-content-center m-auto">
+          <i class="fa-solid fa-star mx-1"></i>
+          <i class="fa-solid fa-star mx-1"></i>
+          <i class="fa-solid fa-star mx-1"></i>
+          <i class="fa-solid fa-star mx-1"></i>
+          <i class="fa-solid fa-star mx-1"></i>
+        </div>
+        <p className="text-center fw-bold">watchmenow</p>
+        <p className="text-center fw-bold orange my-3">$99</p>
+      </div>
+    </NavLink>
+  );
+}
